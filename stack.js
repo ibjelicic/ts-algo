3de349dc1ae8e5f89e9752953392e76facb80b71
Ivan Bjelicic <ibjelicic@users.noreply.github.com>
2024-06-19 09:34:08 +0200

    feat: leetcode 27

--- stack.js
+++ stack.js
@@ -22,9 +22,8 @@ class Stack {
     pop() {
         // return top most element in the stack
         // and removes it from the stack
-        // Underflow if stack is empty
         if (this.items.length == 0)
-            return 'Underflow'
+            return 'nope'
         return this.items.pop();
     }
 
